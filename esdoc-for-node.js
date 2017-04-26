ef17dfd707d2360e09f1f3358eca9b93d21f3d03
Christian Hubinger <christian@trigo.at>
2017-04-27 00:51:57 +0200

    chore: refacte CompositState to be a class & complete API documentation using esdoc

---
+++ esdoc-for-node.js
@@ -0,0 +1,5 @@
+exports.onHandleCode = (ev) => {
+	ev.data.code = ev.data.code // eslint-disable-line
+		.replace(/module\.exports = /g, 'export default ')
+		.replace(/exports = /g, 'export default ');
+};
